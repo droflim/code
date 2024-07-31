1e002ff4dd20e674856b5689592b79f12adf146b
droflim <45541855+droflim@users.noreply.github.com>
2024-07-31 03:04:03 -0400

    Update line.js

--- line.js
+++ line.js
@@ -1,16 +1,9 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({
-    headless: true, // Ejecutar en modo headless
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://html5-chat.com/chat/48967/65cace86434d3/pedrorock');
-  
-  // Hacer algo con la página, como tomar una captura de pantalla
-  await page.screenshot({ path: 'screenshot.png' });
-
+  await page.goto('https://example.com');
+  console.log('Page title:', await page.title());
   await browser.close();
 })();
