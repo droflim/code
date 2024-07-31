81686d24bfaa5a6b0572b2e51a49eb83dc88a6ec
droflim <45541855+droflim@users.noreply.github.com>
2024-07-31 03:04:50 -0400

    Update line.js

--- line.js
+++ line.js
@@ -3,7 +3,14 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
+  await page.goto('https://html5-chat.com/chat/48967/65cace86434d3/pedro', { waitUntil: 'networkidle2' });
+  
+  // Tomar una captura de pantalla de la página
+  await page.screenshot({ path: 'screenshot.png' });
+  
+  // Obtener el título de la página
   console.log('Page title:', await page.title());
+
+  // Cerrar el navegador
   await browser.close();
 })();
